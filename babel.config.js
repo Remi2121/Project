39137fd74b7079a7238a5f5d7cd98e0d8f44f917
Remi <remiluke12@gmail.com>
2025-09-28 09:43:53 +0530

    some changes

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,9 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'], // ✅ Keep this for Reanimated support
+    plugins: [
+      'react-native-worklets/plugin' // ✅ use this instead of reanimated/plugin
+    ],
   };
 };
